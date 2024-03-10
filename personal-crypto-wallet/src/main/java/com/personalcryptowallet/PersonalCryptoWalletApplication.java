638c8791ecf49af4bd4b0eef8fcb8c13ca4d7c0f
Emmanoel <emmanoel_95@hotmail.com>
2024-03-10 15:02:52 -0300

    build: base project

---
+++ personal-crypto-wallet/src/main/java/com/personalcryptowallet/PersonalCryptoWalletApplication.java
@@ -0,0 +1,13 @@
+package com.personalcryptowallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCryptoWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCryptoWalletApplication.class, args);
+	}
+
+}
---
+++ personal-crypto-wallet/src/test/java/com/personalcryptowallet/PersonalCryptoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalcryptowallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCryptoWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
