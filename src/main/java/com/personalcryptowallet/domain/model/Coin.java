408a58f5e3ab6225600a142c27179396f47f7c4b
Emmanoel <emmanoel_95@hotmail.com>
2024-03-10 17:06:58 -0300

    feat(Coin): add Coin model

---
+++ src/main/java/com/personalcryptowallet/domain/model/Coin.java
@@ -0,0 +1,41 @@
+package com.personalcryptowallet.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String tokenAcronym;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private BigDecimal quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime dateContribution;
+
+    @Column(nullable = false)
+    private BigDecimal broker_fee;
+
+}
