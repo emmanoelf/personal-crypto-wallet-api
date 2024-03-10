8004701cc475ba0d93eba61e58dfc27abd0b4bfc
Emmanoel <emmanoel_95@hotmail.com>
2024-03-10 17:07:30 -0300

    feat(Wallet): add Wallet model

---
+++ src/main/java/com/personalcryptowallet/domain/model/Wallet.java
@@ -0,0 +1,46 @@
+package com.personalcryptowallet.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private BigDecimal totalContribuition;
+
+    @Column(nullable = false)
+    private BigDecimal profit;
+
+    @ManyToMany
+    @JoinTable(name = "wallet_coin",
+            joinColumns = @JoinColumn(name = "wallet_id"),
+            inverseJoinColumns = @JoinColumn(name = "coin_id"))
+    private Set<Coin> coins = new HashSet<>();
+
+}
