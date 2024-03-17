1ff2ec1507c53cd629f365b831e590e2e8880dfa
Emmanoel <emmanoel_95@hotmail.com>
2024-03-16 22:43:14 -0300

    feat(EntidadeNaoEncontradaException): add new exception to not found entity

---
+++ src/main/java/com/personalcryptowallet/exceptions/EntidadeNaoEncontradaException.java
@@ -0,0 +1,9 @@
+package com.personalcryptowallet.exceptions;
+
+public class EntidadeNaoEncontradaException extends RuntimeException{
+
+    public EntidadeNaoEncontradaException(String message){
+        super(message);
+    }
+
+}
