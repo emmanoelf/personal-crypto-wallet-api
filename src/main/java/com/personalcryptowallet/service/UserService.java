e8c34df5d0f6f1ddf6d5219ffd1a34a5db202dc8
Emmanoel <emmanoel_95@hotmail.com>
2024-03-16 22:43:36 -0300

    feat(UserService): add method to findById

--- src/main/java/com/personalcryptowallet/service/UserService.java
+++ src/main/java/com/personalcryptowallet/service/UserService.java
@@ -3,6 +3,9 @@ package com.personalcryptowallet.service;
 import com.personalcryptowallet.dto.UserDto;
 import com.personalcryptowallet.dto.UserResponseDto;
 
+import java.util.UUID;
+
 public interface UserService {
     UserResponseDto save(UserDto userDto);
+    UserResponseDto findUser(UUID userId);
 }
--- src/main/java/com/personalcryptowallet/service/impl/UserServicesImpl.java
+++ src/main/java/com/personalcryptowallet/service/impl/UserServicesImpl.java
@@ -2,12 +2,16 @@ package com.personalcryptowallet.service.impl;
 
 import com.personalcryptowallet.dto.UserDto;
 import com.personalcryptowallet.dto.UserResponseDto;
+import com.personalcryptowallet.exceptions.EntidadeNaoEncontradaException;
 import com.personalcryptowallet.mapper.UserMapper;
 import com.personalcryptowallet.model.User;
 import com.personalcryptowallet.repository.UserRepository;
 import com.personalcryptowallet.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,4 +27,14 @@ public class UserServicesImpl implements UserService {
         return UserMapper.toDto(user);
     }
 
+    @Override
+    public UserResponseDto findUser(UUID userId) {
+        Optional<User> user = this.userRepository.findById(userId);
+
+        if(user.isEmpty()){
+            throw new EntidadeNaoEncontradaException("Usuário não encontrado");
+        }
+
+        return UserMapper.toDto(user.get());
+    }
 }
