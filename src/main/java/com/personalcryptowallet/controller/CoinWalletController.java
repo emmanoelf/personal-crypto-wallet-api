6f8153087fd3c94d42645540f20cdb044cae9e8f
Emmanoel <emmanoel_95@hotmail.com>
2024-04-15 02:06:38 -0300

    feat(CoinWalletController): add endpoint to create coin and bound with an wallet

--- src/main/java/com/personalcryptowallet/controller/CoinWalletController.java
+++ src/main/java/com/personalcryptowallet/controller/CoinWalletController.java
@@ -1,8 +1,13 @@
 package com.personalcryptowallet.controller;
 
+import com.personalcryptowallet.dto.CoinDto;
+import com.personalcryptowallet.dto.CoinResponseDto;
 import com.personalcryptowallet.dto.coinMarketCap.CoinMarketCapResponseDto;
 import org.springframework.http.ResponseEntity;
 
+import java.util.UUID;
+
 public interface CoinWalletController {
     ResponseEntity<CoinMarketCapResponseDto> getCoin(String slug);
+    ResponseEntity<CoinResponseDto> addCoin(UUID walletId, CoinDto coinDto);
 }
--- src/main/java/com/personalcryptowallet/controller/impl/CoinWalletControllerImpl.java
+++ src/main/java/com/personalcryptowallet/controller/impl/CoinWalletControllerImpl.java
@@ -1,28 +1,36 @@
 package com.personalcryptowallet.controller.impl;
 
 import com.personalcryptowallet.controller.CoinWalletController;
+import com.personalcryptowallet.dto.CoinDto;
+import com.personalcryptowallet.dto.CoinResponseDto;
 import com.personalcryptowallet.dto.coinMarketCap.CoinMarketCapResponseDto;
 import com.personalcryptowallet.service.CoinWalletService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/v1/wallet/coin")
+@RequestMapping("/v1/wallet/{walletId}")
 public class CoinWalletControllerImpl implements CoinWalletController {
 
-    private CoinWalletService coinWalletService;
+    private final CoinWalletService coinWalletService;
 
     public CoinWalletControllerImpl(CoinWalletService coinWalletService){
         this.coinWalletService = coinWalletService;
     }
 
     @Override
-    @GetMapping
+    @GetMapping(path = "/coin", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CoinMarketCapResponseDto> getCoin(@RequestParam String slug) {
         return ResponseEntity.status(HttpStatus.OK).body(this.coinWalletService.getCoin(slug));
+    }
+
+    @Override
+    @PostMapping(path = "/coin", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CoinResponseDto> addCoin(@PathVariable UUID walletId, @RequestBody CoinDto coinDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.coinWalletService.addCoin(walletId, coinDto));
     }
 }
