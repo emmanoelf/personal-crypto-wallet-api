7b5c7c1e60f2a994fcb310440d218dfb16f27453
Emmanoel <emmanoel_95@hotmail.com>
2024-04-15 02:05:50 -0300

    feat(CoinWalletService): add method to create a coin and bound with an wallet

--- src/main/java/com/personalcryptowallet/service/CoinWalletService.java
+++ src/main/java/com/personalcryptowallet/service/CoinWalletService.java
@@ -1,7 +1,12 @@
 package com.personalcryptowallet.service;
 
+import com.personalcryptowallet.dto.CoinDto;
+import com.personalcryptowallet.dto.CoinResponseDto;
 import com.personalcryptowallet.dto.coinMarketCap.CoinMarketCapResponseDto;
+
+import java.util.UUID;
 
 public interface CoinWalletService {
     CoinMarketCapResponseDto getCoin(String slug);
+    CoinResponseDto addCoin(UUID walletId, CoinDto coinDto);
 }
--- src/main/java/com/personalcryptowallet/service/impl/CoinWalletServiceImpl.java
+++ src/main/java/com/personalcryptowallet/service/impl/CoinWalletServiceImpl.java
@@ -1,19 +1,43 @@
 package com.personalcryptowallet.service.impl;
 
 import com.personalcryptowallet.client.CoinMarketCapClient;
+import com.personalcryptowallet.dto.CoinDto;
+import com.personalcryptowallet.dto.CoinResponseDto;
 import com.personalcryptowallet.dto.coinMarketCap.CoinMarketCapResponseDto;
+import com.personalcryptowallet.mapper.CoinMapper;
+import com.personalcryptowallet.model.Coin;
+import com.personalcryptowallet.model.Wallet;
+import com.personalcryptowallet.repository.CoinRepository;
+import com.personalcryptowallet.repository.WalletRepository;
 import com.personalcryptowallet.service.CoinWalletService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class CoinWalletServiceImpl implements CoinWalletService {
 
     private final CoinMarketCapClient coinMarketCapClient;
+    private final CoinRepository coinRepository;
+    private final WalletRepository walletRepository;
 
     @Override
     public CoinMarketCapResponseDto getCoin(String slug) {
         return this.coinMarketCapClient.getCoin(slug);
+    }
+
+    @Override
+    public CoinResponseDto addCoin(UUID walletId, CoinDto coinDto) {
+        Coin coin = CoinMapper.toModel(coinDto);
+
+        Optional<Wallet> wallet = this.walletRepository.findById(walletId);
+        wallet.get().getCoins().add(coin);
+
+        Coin savedCoin = this.coinRepository.save(coin);
+
+        return CoinMapper.toDto(savedCoin);
     }
 }
