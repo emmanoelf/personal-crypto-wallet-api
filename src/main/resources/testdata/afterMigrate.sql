571ea746a7836ad007bc5d5723f8a7aef4b30035
Emmanoel <emmanoel_95@hotmail.com>
2024-04-16 21:53:44 -0300

    build(afterMigrate): add data to initialize table user

--- src/main/resources/testdata/afterMigrate.sql
+++ src/main/resources/testdata/afterMigrate.sql
@@ -1,8 +1,11 @@
 set foreign_key_checks = 0;
 
 delete from coin;
+delete from user;
 
 set foreign_key_checks = 1;
 
 insert into coin (id, idCoinMarketCap, slug, symbol, price, quantity, dateContribution)
 values('bad71db7-0028-49af-951c-a5695cf44df8', 1, 'bitcoin', 'BTC', 25391.49, 0.00122211, '2022-06-22T10:15:30');
+
+insert into user(id, email, password) values ('f2558033-f618-4a9f-87e5-2e799b178808', 'test@test.com', '1234');
